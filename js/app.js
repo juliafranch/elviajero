aac139e7022feb0c208b27d0a9b0d3367b8f3253
Julià Franch Escuer <juliafranch@gmail.com>
2017-05-23 20:29:34 +0200

    Ajustaments de la home i el menú. El botó de menú ja funciona.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
+    $(".menuicon").click(function() {
+        $(".menu").toggleClass("open");
+    });
+});
 
+var scrollAnterior = 0;
+
+$(document).scroll(function() {
+  var scrollActual = $(document).scrollTop();
+
+  if (scrollActual > 20) {
+      $(".menubar").css("height","55px");
+  } else {
+      $(".menubar").css("height","75px");
+  }
 });
