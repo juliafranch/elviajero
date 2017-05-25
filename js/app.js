23cca1ba3ec2c7fe23c99ca1b8a8d75db30225e4
Julià Franch Escuer <juliafranch@gmail.com>
2017-05-25 18:20:50 +0200

    Responsive tablet millorat.

--- js/app.js
+++ js/app.js
@@ -13,15 +13,18 @@ var scrollAnterior = 0;
 $(document).scroll(function() {
   var scrollActual = $(document).scrollTop();
 
-  if (scrollActual > 20) {
-      $(".menubar").css("height","55px");
-  } else {
-      $(".menubar").css("height","75px");
-  }
+  if ($(window).width()<768) {
 
-  if($(window).scrollTop() + $(window).height() == $(document).height()) {
-       $(".menubar").css("height","0");
-   }
+      if (scrollActual > 20) {
+          $(".menubar").css("height","55px");
+      } else {
+          $(".menubar").css("height","75px");
+      }
+
+      if($(window).scrollTop() + $(window).height() == $(document).height()) {
+           $(".menubar").css("height","0");
+       }
+  }
 
 });
 
